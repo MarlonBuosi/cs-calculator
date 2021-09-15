4a2055d9504240a95b280205721f382b9ceb10cc
MarlonBuosi <marlonbuosi_@hotmail.com>
2021-09-15 19:32:59 -0300

    inital commit

---
+++ Exercício1/Calculos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício1
+{
+    class Calculos
+    {
+        private int numero;
+        private float numeroRecebido;
+        private int expoenteRecebido;
+        private float numeroRecebido2;
+        private float porcentagemInt;
+        private float nase;
+        private float altura;
+        public int Fatorial()
+        {
+            int cont;
+            int resultado;
+
+            cont = numero - 1;
+            resultado = numero * cont;
+            cont--;
+
+            do
+            {
+                resultado *= cont;
+                cont--;
+            }
+            while (cont != 1);
+
+            return resultado;
+        }
+
+        public float Exponenciacao()
+        {
+            int expoente = 1;
+            float numero2;
+            numero2 = numeroRecebido;
+            do
+            {
+                numeroRecebido *= numero2;
+                expoente++;
+            }
+            while (expoente != expoenteRecebido);
+
+            return numeroRecebido;
+        }
+
+        public float Porcentagem()
+        {
+            float porcentagemDecimal;
+            float resultado;
+
+            porcentagemDecimal = porcentagemInt / 100;
+
+            resultado = numeroRecebido2 * porcentagemDecimal;
+
+            return (resultado);
+        }
+
+        public float Area()
+        {
+            float area;
+
+            area = (nase * altura) / 2;
+
+            return area;
+        }
+
+        public Calculos() { this.Numero = numero; }
+        public int Numero { get { return numero; } set { numero = value; } }
+        public int ExpoenteRecebido { get { return expoenteRecebido; } set { expoenteRecebido = value; } }
+        public float NumeroRecebido { get { return numeroRecebido; } set { numeroRecebido = value; } }
+        public float NumeroRecebido2 { get { return numeroRecebido2; } set { numeroRecebido2 = value; } }
+        public float PorcentagemInt { get { return porcentagemInt; } set { porcentagemInt = value; } }
+        public float Nase { get { return nase; } set { nase = value; } }
+        public float Altura { get { return altura; } set { altura = value; } }
+
+    }
+
+
+}
---
+++ Exercício1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Exercício1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int menu;
+
+            do
+            {
+                Console.WriteLine("Digite uma das opções abaixo:\n");
+                Console.WriteLine("1) Fatorial");
+                Console.WriteLine("2) Exponenciação");
+                Console.WriteLine("3) Porcentagem");
+                Console.WriteLine("4) Área do triângulo");
+                Console.WriteLine("5) Sair.");
+
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.Clear();
+                        Calculos c1 = new ();
+                        Console.Write("Entre o número que você deseja calcular o fatorial: ");
+                        c1.Numero = int.Parse(Console.ReadLine());
+                        Console.Write("O resultado do fatorial recebido é: " + c1.Fatorial() + "\n\n");
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Calculos c2 = new ();
+                        Console.Write("Entre o número que você deseja calcular a exponenciação: ");
+                        c2.NumeroRecebido = float.Parse(Console.ReadLine());
+                        Console.Write("Entre o expoente do número: ");
+                        c2.ExpoenteRecebido = int.Parse(Console.ReadLine());
+                        Console.WriteLine("O resultado da exponenciação é: " + c2.Exponenciacao() + "\n");
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Calculos c3 = new ();
+                        float numero;
+                        float porcentagem;
+                        Console.Write("Entre o número que você deseja calcular a porcentagem: ");
+                        c3.NumeroRecebido2 = float.Parse(Console.ReadLine());
+                        Console.Write("Entre a porcentagem: ");
+                        c3.PorcentagemInt = float.Parse(Console.ReadLine());
+                        numero = c3.NumeroRecebido2;
+                        porcentagem = c3.PorcentagemInt;
+                        Console.WriteLine("O número " + c3.Porcentagem() + " é " + porcentagem + "% de " + numero + ".\n");
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Calculos c4 = new();
+                        Console.Write("Entre a base do triângulo: ");
+                        c4.Nase = float.Parse(Console.ReadLine());
+                        Console.Write("Entre a altura do triângulo: ");
+                        c4.Altura = float.Parse(Console.ReadLine());
+                        Console.WriteLine("A área do triângulo é: " + c4.Area() + "\n");
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Entre uma Opção Válida.\n");
+                        break;
+                }
+            }
+            while (menu != 5);
+        }
+    }
+}
